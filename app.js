cb642e94fc915d75d69e6e54109da378f0ad6c9a
SaadDev <zwixozouine@gmail.com>
2026-03-29 21:44:58 +0100

    .env and ejs config

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { configDotenv } from "dotenv";
+import dotenv from "dotenv";
 
+dotenv.config({ path: './.env' })
 
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/",(req, res)=>{
     res.send("Hello World!");   
